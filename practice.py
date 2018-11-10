f0c991c901d8192b5716234f0e9de19b3a4efade
bulkin59 <44915975+bulkin59@users.noreply.github.com>
2018-11-10 18:19:47 +0900

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,32 @@
+# data type
+# data conversion
+
+# int 10.XX -> 10
+a = 10.3
+b = 10.7
+a = int(a)
+b = int(b)
+print(a+b)
+
+# float 10 -> 10.X
+a = 10.3
+b = 10
+a = float(a)
+b = float(b)
+print(a)
+print(b)
+print(a+b)
+
+# str  "--"
+a = float(76.3)
+b = a
+print(a+b)
+
+a = str(a)
+b = str(b)
+print(a+b)
+
+# find type
+type(a)
+type(b)
+
