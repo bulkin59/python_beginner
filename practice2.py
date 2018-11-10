ea1fc9bf064749e7f1c803c215b739bf87823175
bulkin59 <44915975+bulkin59@users.noreply.github.com>
2018-11-10 19:43:52 +0900

    데이터형을 변환해보자

--- practice2.py
+++ practice2.py
@@ -1,3 +1,20 @@
+
+
+# input -> string 값으로 인식된다.
+
+print("Enter your name!", "Hello People~")
+somebody = input()
+print("Hi", somebody)
+
+
+# input -> float 값으로 변환해본다.
+
+temperature = float(input("온도를 입력하세요 :"))
+print(temperature)
+print(type(temperature))
+
+
+# 응용해보자
 # 화씨변환기 프로그램
 
 print("본 프로그램은 섭씨를 화씨로 변환해주는 프로그램입니다.")
@@ -6,16 +23,3 @@ c_temp=float(input())
 f_temp=((9/5)*c_temp)+32
 print("섭씨온도 : ", c_temp)
 print("화씨온도 : ", f_temp)
-
-# input -> string
-
-print("Enter your name!", "Hello People~")
-somebody = input()
-print("Hi", somebody)
-
-# input -> float
-temperature = float(input("온도를 입력하세요 :"))
-print(temperature)
-print(type(temperature))
-
-
